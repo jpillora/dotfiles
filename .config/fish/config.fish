bc010fd58371aab3c05a1895f6ac139eec1dfdf1
Jaime Pillora <dev@jpillora.com>
2022-04-29 23:00:46 +1000

    init

---
+++ .config/fish/config.fish
@@ -0,0 +1,39 @@
+set -x GOPATH $HOME/Code/Go
+set PATH $HOME/bin /usr/local/bin /usr/local/go/bin $GOPATH/bin $PATH
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+end
+
+function fish_prompt
+  set_color cyan
+  echo -n $USER
+  echo -n ' '
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color normal
+  echo -n ' '
+end
+
+function prompt_pwd --description 'modify pwd to shorten paths, add git branch'
+  set wd "$PWD"
+  set wd (string replace "$HOME" "~" "$wd")
+  set_color green
+  echo -n "$wd"
+  set_color normal
+  if set branch (git rev-parse --abbrev-ref HEAD 2> /dev/null);
+    echo -n " ("
+    set_color yellow
+    echo -n "$branch"
+    set_color normal
+    echo -n ")"
+  end
+  echo ""
+end
+
+function l
+  exa $argv
+end
---
+++ README.md
@@ -0,0 +1,13 @@
+# dotfiles
+
+My configuration for:
+
+* fish
+* tmux
+* ssh
+
+Plus a some handy scripts
+
+TODO
+
+* bare git repo with script to edit
